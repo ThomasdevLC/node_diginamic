05732e7b4332da9bc11a6acabdfdf1775090325a
ThomasdevLC <thomas.lecam@gmail.com>
2024-04-11 16:38:53 +0200

    test

--- mouli_es6/NODE/first-app/app.js
+++ mouli_es6/NODE/first-app/app.js
@@ -2,10 +2,12 @@ const express = require("express");
 
 const app = express();
 
-app.listen(3000, () => {
+app.listen(8000, () => {
   console.log("App running on port 3000");
 });
 
 app.get("/", (req, res) => {
-  res.send("Hello world!");
+  res.send("Hello world! " + "Hello world!");
 });
+
+//testing
